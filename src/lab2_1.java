7b5080f5c922a6693d5417783a2073209397966a
4Rinzler4 <127138464+4Rinzler4@users.noreply.github.com>
2024-05-11 11:54:57 +0300

    Update lab2_1.java

--- src/lab2_1.java
+++ src/lab2_1.java
@@ -1,24 +1,46 @@
+import java.util.InputMismatchException;
 import java.util.Scanner;
-public class lab2_1{
+
+public class lab2_1 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        double x = 0, y = 0;
-        boolean validInput = false;
-        while (!validInput) {
-            System.out.print("Введіть значення x: ");
-            x = scanner.nextDouble();
-            System.out.print("Введіть значення y: ");
-            y = scanner.nextDouble();
+        boolean continueProgram = true;
 
-            if (x != 0 && x != -2 && Math.exp(-x) != 2) {
-                validInput = true;
-            } else {
-                System.out.println("Увага: Для заданих значень x та y вираз виконується ділення на 0 або отримання невизначеного результату. Спробуйте інші значення.");
+        while (continueProgram) {
+            double x = 0, y = 0;
+            boolean validInput = false;
+
+            while (!validInput) {
+                try {
+                    System.out.print("Введіть значення x: ");
+                    x = scanner.nextDouble();
+                    System.out.print("Введіть значення y: ");
+                    y = scanner.nextDouble();
+
+                    if (isValidInput(x, y)) {
+                        validInput = true;
+                    } else {
+                        System.out.println("Увага: Для заданих значень x та y вираз виконується ділення на 0 або отримання невизначеного результату. Спробуйте інші значення.");
+                    }
+                } catch (InputMismatchException e) {
+                    System.out.println("Помилка: Введені значення повинні бути числами.");
+                    scanner.nextLine();
+                }
             }
+            double result = calculateFunction(x, y);
+            System.out.printf("Значення функції a: %.2f%n", result);
+
+            System.out.print("Бажаєте продовжити роботу (так/ні)? ");
+            String choice = scanner.next();
+            continueProgram = choice.equalsIgnoreCase("так");
         }
-        double result = calculateFunction(x, y);
-        System.out.printf("Значення функції a: %.2f%n", result);
+        scanner.close();
     }
+
+    public static boolean isValidInput(double x, double y) {
+        return x != 0 && x != -2 && Math.exp(-x) != 2 && !Double.isNaN(y);
+    }
+
     public static double calculateFunction(double x, double y) {
         double a = (1 + y) * (((x + (y / (Math.pow(x, 2) + 4))) / (Math.exp(-x) - 2)) + (1 / (Math.pow(x, 2) + 4)));
         return a;
