dbddcb489f7e216f2df80cd58466ea40d573cfc4
4Rinzler4 <127138464+4Rinzler4@users.noreply.github.com>
2024-05-11 11:55:41 +0300

    Update lab2_4.java

--- src/lab2_4.java
+++ src/lab2_4.java
@@ -1,36 +1,48 @@
+import java.util.Scanner;
+
 public class lab2_4 {
     public static void main(String[] args) {
-        double a = -Math.PI / 2;
-        double b = Math.PI / 2;
-        double dx = Math.PI / 30;
-        double x = a;
-        double y;
+        Scanner scanner = new Scanner(System.in);
+        boolean continueProgram = true;
 
-        System.out.println("  x     |  f(x) ");
-        System.out.println("--------|--------");
+        while (continueProgram) {
+            double a = -Math.PI / 2;
+            double b = Math.PI / 2;
+            double dx = Math.PI / 30;
+            double x = a;
+            double y;
 
-        while (x <= b) {
-            if (x == 1) {
-                System.out.printf("%7.2f | Невизначено%n", x);
-            } else {
-                y = Math.sin(x) / x;
-                System.out.printf("%7.2f | %7.2f%n", x, y);
+            System.out.println("  x     |  f(x) ");
+            System.out.println("--------|--------");
+
+            while (x <= b) {
+                if (x == 1) {
+                    System.out.printf("%7.2f | Невизначено%n", x);
+                } else {
+                    y = Math.sin(x) / x;
+                    System.out.printf("%7.2f | %7.2f%n", x, y);
+                }
+                x += dx;
             }
-            x += dx;
+
+            System.out.println("  x     |  f(x) ");
+            System.out.println("-----------------");
+
+            x = a;
+            do {
+                if (x == 2) {
+                    System.out.printf("%7.2f | Невизначено%n", x);
+                } else {
+                    y = Math.sin(x) / x;
+                    System.out.printf("%7.2f | %7.2f%n", x, y);
+                }
+                x += dx;
+            } while (x <= b);
+
+            System.out.print("Бажаєте продовжити роботу (так/ні)? ");
+            String choice = scanner.next();
+            continueProgram = choice.equalsIgnoreCase("так");
         }
-
-        System.out.println("  x     |  f(x) ");
-        System.out.println("-----------------");
-
-        x = a;
-        do {
-            if (x == 2) {
-                System.out.printf("%7.2f | Невизначено%n", x);
-            } else {
-                y = Math.sin(x) / x;
-                System.out.printf("%7.2f | %7.2f%n", x, y);
-            }
-            x += dx;
-        } while (x <= b);
+        scanner.close();
     }
 }
